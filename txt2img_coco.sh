25712923aae33ce3573ac911d31af02a4430d127
Leonardo <swf_developer@outlook.com>
2025-05-08 21:00:18 -0300

    adding txt2img_coco.sh

--- txt2img_coco.sh
+++ txt2img_coco.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 python txt2img_coco.py \
         --ddim_eta 0.0 \
-        --n_samples 1 \
+        --n_samples 8 \
         --n_iter 1 \
         --scale 7.5 \
-        --ddim_steps 30 \
+        --ddim_steps 50 \
         --plms \
         --skip_grid \
         --ckpt /home/swf_developer/storage/checkpoints/stable_diffusion/sd-v1-4-full-ema.ckpt \
