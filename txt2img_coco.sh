a01c390dd205eda0353fa4d96b4daf3e8f17ca2d
Leonardo <swf_developer@outlook.com>
2025-05-10 09:43:39 -0300

    adding txt2img_coco.sh

--- txt2img_coco.sh
+++ txt2img_coco.sh
@@ -8,10 +8,8 @@ python txt2img_coco.py \
         --plms \
         --skip_grid \
         --ckpt /home/swf_developer/storage/checkpoints/stable_diffusion/sd-v1-4-full-ema.ckpt \
-        --from-file '/home/swf_developer/stable-diffusion/scripts/coco_captions_5000_resume.txt' \
+        --from-file '/home/swf_developer/stable-diffusion/scripts/coco_captions_5000.txt' \
         --outdir '/home/swf_developer/storage/attack/targeted_images' \
-
-
 
         
 
