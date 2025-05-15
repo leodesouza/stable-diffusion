a07c337c0254bd27994dd5f4e63e4f39279567d0
Leonardo <swf_developer@outlook.com>
2025-05-15 20:28:20 -0300

    adding txt2img_coco.sh

--- download_imagenet.py
+++ download_imagenet.py
@@ -3,8 +3,11 @@ from PIL import Image
 
 # Load first 10,000 images from training set
 dataset = load_dataset("imagenet-1k", split="train[:5000]")
+dataset = dataset.shuffle(seed=42)
 
 # Save them to disk (optional)
 for i, example in enumerate(dataset):
     image = example['image']  # This is a PIL.Image
-    image.save(f"/home/swf_developer/storage/attack/clean_images/img_{i:05d}.jpg")
+    file_name = f"img_{i:05d}"
+    print(f'saving file: {file_name}')
+    image.save(f"/home/swf_developer/storage/attack/clean_images/{file_name}.jpg")
