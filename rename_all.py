c7d67ede17bd0fc7748a53d2cbbcce772d9979d6
Leonardo <swf_developer@outlook.com>
2025-05-15 12:10:29 -0300

    adding txt2img_coco.sh

--- rename_all.py
+++ rename_all.py
@@ -1,15 +1,11 @@
 import os
 
-# Folder containing the PNG files
-folder_path = '/home/swf_developer/storage/attack/targeted_images/samples/'  # Change this to your folder path
 
-# Starting count
-start_count = 4391
-
-# Get list of .png files sorted (e.g., alphabetically)
+folder_path = '/home/swf_developer/storage/attack/targeted_images/samples/'
+start_count = 2347
 png_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')])
 
-# Rename files
+
 for i, filename in enumerate(png_files):
     new_name = f"{start_count + i:05}.png"  # Format as 5-digit number
     src = os.path.join(folder_path, filename)
